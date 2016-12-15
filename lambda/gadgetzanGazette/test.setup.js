fe162fa835398d7d2ac7e02620322635b980c71b
Greg Stewart <gregs@tcias.co.uk>
2016-12-15 20:47:00 +0000

    Setup mocha

---
+++ lambda/gadgetzanGazette/test.setup.js
@@ -0,0 +1,4 @@
+import chai from 'chai';
+import 'babel-polyfill';
+
+global.expect = chai.expect;
---
+++ lambda/gadgetzanGazette/test/sample.test.js
@@ -0,0 +1,5 @@
+describe('sample test', () => {
+  it('runs a simple test', () => {
+    expect(1+1).to.equal(2);
+  });
+});
