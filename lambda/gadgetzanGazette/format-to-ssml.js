0fcddd93f51d6ac428e0631aaa9da68371ec7a94
Greg Stewart <gregs@tcias.co.uk>
2016-12-23 11:43:15 +0100

    Fixes #6: add game title

--- lambda/gadgetzanGazette/format-to-ssml.js
+++ lambda/gadgetzanGazette/format-to-ssml.js
@@ -3,7 +3,7 @@ export const formatToSSML = (items) => {
   const source = `<p>Here is the news from Icy Veins</p>`;
 
   const itemsToBeSpoken = items.map((item) => {
-    return `<p>${item.title.replace(/&/g, 'and')}</p><p>${item.description.replace(/&/g, 'and')}</p>`;
+    return `<p>In ${item.game} news: ${item.title.replace(/&/g, 'and')}</p><p>${item.description.replace(/&/g, 'and')}</p>`;
   });
 
   return `<speak>${intro}${source}${itemsToBeSpoken.join('')}</speak>`;
