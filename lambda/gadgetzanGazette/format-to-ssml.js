fd2130155de4c5de89a1e4768675476cddc99df3
Greg Stewart <gregs@tcias.co.uk>
2016-12-23 11:54:35 +0100

    Fixes #5: Add concluding text

--- lambda/gadgetzanGazette/format-to-ssml.js
+++ lambda/gadgetzanGazette/format-to-ssml.js
@@ -1,10 +1,11 @@
 export const formatToSSML = (items) => {
   const intro = `<p>Welcome to the Gadgetzan Gazette!</p>`;
-  const source = `<p>Here is the news from Icy Veins</p>`;
+  const source = `<p>Here is the news from Icy Veins.</p>`;
+  const conclusion = `<p>This concludes our news roundup.</p>`;
 
   const itemsToBeSpoken = items.map((item) => {
     return `<p>In ${item.game} news: ${item.title.replace(/&/g, 'and')}</p><p>${item.description.replace(/&/g, 'and')}</p>`;
   });
 
-  return `<speak>${intro}${source}${itemsToBeSpoken.join('')}</speak>`;
+  return `<speak>${intro}${source}${itemsToBeSpoken.join('')}${conclusion}</speak>`;
 }
