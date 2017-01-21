8b0c49dc681a4ea69baa0438e2787d461a3e1d97
Greg Stewart <gregs@tcias.co.uk>
2017-01-21 10:58:35 +0000

    Limit output to under 8000 characters

--- lambda/gadgetzanGazette/format-to-ssml.js
+++ lambda/gadgetzanGazette/format-to-ssml.js
@@ -7,9 +7,19 @@ export const formatToSSML = (items) => {
   const source = `<p>Here is the news from Icy Veins.</p>`;
   const conclusion = `<p>This concludes our news roundup.</p>`;
 
-  const itemsToBeSpoken = items.map((item) => {
+  const itemsToBeSpokenAsFormattedArray = items.map((item) => {
     return `<p>In ${item.game} news: ${item.title.replace(/&/g, 'and')}</p><p>${item.description.replace(/&/g, 'and')}</p>`;
   });
 
-  return `<speak>${intro}${source}${itemsToBeSpoken.join('')}${conclusion}</speak>`;
+  const buildResponse = (itemsToBeSpoken) => {
+    let response = `<speak>${intro}${source}${itemsToBeSpoken.join('')}${conclusion}</speak>`;
+
+    if (response.length >= 8000) {
+      return buildResponse(itemsToBeSpoken.slice(0,itemsToBeSpoken.length-1));
+    }
+
+    return response;
+  }
+
+  return buildResponse(itemsToBeSpokenAsFormattedArray);
 }
