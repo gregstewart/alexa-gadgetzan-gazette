fcb2ea93251d9fd24d4f23189448009c65502416
Greg Stewart <gregs@tcias.co.uk>
2017-06-06 16:36:39 +0100

    switch to ARN and new skill name

--- lambda/blizzard-news-flash/handler.js
+++ lambda/blizzard-news-flash/handler.js
@@ -2,18 +2,20 @@
 
 module.exports.hello = (event, context, callback) => {
   const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      version: '1.0',
-      response: {
-        shouldEndSession: true,
-        outputSpeech: {
-          type: 'SSML',
-          ssml: 'Go Serverless v1.0! Your function executed successfully!'
-        }
-      }
-    }),
-  };
-
-  callback(null, response);
+    "version": "1.0",
+    "response": {
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": "Alexa responds with this text"
+      },
+      "card": {
+        "content": "Message for the Alexa companion app.",
+        "title": "Title for the Message",
+        "type": "Simple"
+      },
+      "shouldEndSession": true
+    },
+    "sessionAttributes": {}
+  }
+  callback(null, response)
 };
